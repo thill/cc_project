b7ffe8bc4f1b5ccee82f6dda7eac64269ed762b7
Eric Thill <code@thill.io>
2021-02-15 18:40:50 -0600

    shared Dockerfile via symlinks

--- gen.sh
+++ gen.sh
@@ -8,11 +8,20 @@ if [ -z "${PROJECT_NAME}" ]; then
   exit -1
 fi
 
-cp -r template/ "${PROJECT_NAME}"
+cp -r template/code "${PROJECT_NAME}"
+cp -r template/ide "${PROJECT_NAME}/ide"
 for f in $(find "${PROJECT_NAME}"); do
   if [ -f "$f" ]; then
     sed -i -e "s/{{{ PROJECT_NAME }}}/${PROJECT_NAME}/g" "$f"
   fi
 done
 
-cd "${PROJECT_NAME}" && git init 
+cd "${PROJECT_NAME}"
+for IDE in ./ide/*; do
+  if [ -d "$IDE" ]; then
+    pushd "${IDE}/.devcontainer"
+    ln -s ../../Dockerfile Dockerfile
+    popd
+  fi
+done
+
